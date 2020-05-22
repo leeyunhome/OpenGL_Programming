1236671b4d7bf4a46f270d80297a1070feefc4b8
leeyunhome <leeyunhome@daum.net>
2020-05-23 08:19:03 +0900

    add drawPixel, drawLine

--- OpenGL1/OpenGL1/OpenGL1.cpp
+++ OpenGL1/OpenGL1/OpenGL1.cpp
@@ -1,5 +1,28 @@
 #include <GLFW/glfw3.h>
+#include <algorithm>
 
+const int width = 50;
+const int height = 50;
+
+float* pixels = new float[width * height * 3];
+
+void drawPixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+{
+    pixels[(i + width * j) * 3 + 0] = red;
+    pixels[(i + width * j) * 3 + 1] = green;
+    pixels[(i + width * j) * 3 + 2] = blue;
+
+}
+
+void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+{
+    for (int i = i0; i <= i1; i++)
+    {
+        const int j = (j1 - j0) * (i - i0) / (i1 - i0) + j0;
+
+        drawPixel(i, j, red, green, blue);
+    }
+}
 int main(void)
 {
     GLFWwindow* window;
@@ -9,7 +32,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(width, height, "Seyeon", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -25,12 +48,19 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        std::fill_n(pixels, width * height * 3, 1.0f);
+        drawPixel(25, 25, 1.0f, 0.0f, 0.0f);
+        drawLine(0, 0, 50, 50, 0.0f, 1.0f, 0.0f);
+        glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    delete[] pixels;
 
     glfwTerminate();
     return 0;
