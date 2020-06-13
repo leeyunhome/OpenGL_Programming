ed54cafb03501adba3b0e256cb77c78d3dfa23b3
leeyunhome <leeyunhome@daum.net>
2020-06-14 07:40:01 +0900

    up to Ins and outs When the types and the names are equal on both sides OpenGL will link those variables together and then it is possible to send data between shaders

--- LearnOpenGL_HelloWindow/LearnOpenGL_HelloWindow/LearnOpenGL_HelloWindow.cpp
+++ LearnOpenGL_HelloWindow/LearnOpenGL_HelloWindow/LearnOpenGL_HelloWindow.cpp
@@ -8,16 +8,20 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"out vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
-	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	//"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"	FragColor = vertexColor;\n"
 	"}\0";
 
 void processInput(GLFWwindow* window);
@@ -49,6 +53,10 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
@@ -58,23 +66,23 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//float vertices[] = {
-		//	-0.5f, -0.5f, 0.0f,
-		//	 0.5f, -0.5f, 0.0f,
-		//	 0.0f,  0.5f, 0.0f
-		//};
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
 
 		// Element Buffer Objects
-		float vertices[] = {
-			 0.5f,  0.5f, 0.0f,  // top right
-			 0.5f, -0.5f, 0.0f,  // bottom right
-			-0.5f, -0.5f, 0.0f,  // bottom left
-			-0.5f,  0.5f, 0.0f   // top left 
-		};
-		unsigned int indices[] = {  // note that we start from 0!
-			0, 1, 3,   // first triangle
-			1, 2, 3    // second triangle
-		};
+		//float vertices[] = {
+		//	 0.5f,  0.5f, 0.0f,  // top right
+		//	 0.5f, -0.5f, 0.0f,  // bottom right
+		//	-0.5f, -0.5f, 0.0f,  // bottom left
+		//	-0.5f,  0.5f, 0.0f   // top left 
+		//};
+		//unsigned int indices[] = {  // note that we start from 0!
+		//	0, 1, 3,   // first triangle
+		//	1, 2, 3    // second triangle
+		//};
 
 
 		unsigned int vertexShader;
@@ -128,8 +136,8 @@ int main()
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 		// 3. copy our index array in a element buffer for OpenGL to use
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 		// 4. then set the vertex attributes pointers
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
@@ -138,9 +146,9 @@ int main()
 		// 4. draw the object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glDeleteShader(vertexShader);
